621f0d82df513e720a8a09c5a3cabee9fe8bac72
Seforah <sef.benkhalifa@gmail.com>
2024-11-12 12:51:54 +0100

    save filesxml

--- LocalizationFilesManager/Core/XMLUtility.cs
+++ LocalizationFilesManager/Core/XMLUtility.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
 
 namespace LocalizationFilesManager
 {
@@ -12,7 +14,17 @@ namespace LocalizationFilesManager
             using (StreamReader reader = new StreamReader(filePath))
             {
                 string content = reader.ReadToEnd();
-                // Process XML data here
+                gridData.Rows.Clear();
+
+                //XmlDocument xmlDoc = new XmlDocument();
+                //xmlDoc.LoadXml(content);
+
+                //XmlNode languagesNode = xmlDoc.SelectSingleNode("/Languages");
+
+                //foreach (XmlNode idNode in languagesNode.SelectNodes("id"))
+                //{
+
+                //}
                 MessageBox.Show("XML Loaded:\n" + content);
             }
         }
@@ -21,9 +33,33 @@ namespace LocalizationFilesManager
         {
             using (StreamWriter writer = new StreamWriter(filePath))
             {
-                string content = ""; // Process XML data here
-                writer.Write(content);
-                MessageBox.Show("XML Loaded:\n" + content);
+                //string content = ""; // Process XML data here
+                //writer.Write(content);
+                XmlWriter xmlWriter = new XmlTextWriter(writer);
+                XmlWriter.Create(xmlWriter);
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("Languages");
+
+                foreach (var row in gridData.Rows)
+                {
+                    xmlWriter.WriteStartElement("id");
+                    xmlWriter.WriteAttributeString("value", row.Key);
+
+                    for (int i = 0; i < gridData.Key.Languages.Count; i++)
+                    {
+                        string language = gridData.Key.Languages[i];
+                        string translation = row.Languages[i];
+                        xmlWriter.WriteElementString(language, translation);
+                    }
+
+                    xmlWriter.WriteElementString("Comments", row.Comments);
+
+                    xmlWriter.WriteEndElement();
+                }
+
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndDocument();
+                MessageBox.Show("XML Loaded:\n" + xmlWriter);
             }
         }
     }
