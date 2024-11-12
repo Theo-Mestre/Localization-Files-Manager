296357e1f6f6dd23e1b4bb0fa32bd62899d1d218
Akanlyfe <kikoukat@hotmail.fr>
2024-11-12 12:57:25 +0100

    Trying to save and read JSON files, still WIP

--- LocalizationFilesManager/Core/JsonUtility.cs
+++ LocalizationFilesManager/Core/JsonUtility.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace LocalizationFilesManager
 {
@@ -12,7 +16,35 @@ namespace LocalizationFilesManager
             using (StreamReader reader = new StreamReader(filePath))
             {
                 string content = reader.ReadToEnd();
-                // Process JSON data here
+                gridData.Rows.Clear();
+
+                /*gridData.Rows.Add(
+                    new RowData
+                    {
+                        Key = key,
+                        Languages = values,
+                        Comments = comment
+                    });*/
+
+                string[] lines = content.Split('\n');
+                foreach (string line in lines)
+                {
+                    /* TODO LIST:
+                     * 
+                     * remove all '"' in the line
+                     * remove the ':' in the line
+                     * 
+                     * store key
+                     * store values
+                     * store comment
+                     * 
+                     * add to Rows
+                     * 
+                     */
+
+                    MessageBox.Show("Line:\n" + line);
+                }
+
                 MessageBox.Show("JSON Loaded:\n" + content);
             }
         }
@@ -21,9 +53,20 @@ namespace LocalizationFilesManager
         {
             using (StreamWriter writer = new StreamWriter(filePath))
             {
-                string content = ""; // Process JSON data here
+                string content = "";
+                List<string> keys = [];
+                List<ObservableCollection<string>> values = [];
+                List<string> comments = [];
+
+                foreach (RowData rowData in gridData.Rows)
+                {
+                    content += "\"" + rowData.Key + "\":\n";
+                    foreach (string value in rowData.Languages)
+                        content += "\"" + value + "\"";
+                }
+
                 writer.Write(content);
-                MessageBox.Show("JSON Loaded:\n" + content);
+                MessageBox.Show("JSON Saved:\n" + content);
             }
         }
     }
