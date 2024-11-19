2bb4d7458106082eabd89efb8b7d5f84723fd06d
kbabel971 <kevin.jordan.babel@gmail.com>
2024-11-19 10:47:59 +0100

    ajout de la sauvergarde csv + modification de l'importation

--- LocalizationFilesManager/Core/CSVUtility.cs
+++ LocalizationFilesManager/Core/CSVUtility.cs
@@ -12,27 +12,30 @@ namespace LocalizationFilesManager
     public partial class MainWindow
     {
         // CSV Utility Methods
-        
+
+        // import
         private void OnCSVFileOpened(string filePath)
         {
             using (StreamReader reader = new StreamReader(filePath))
             {
-               // string content = reader.ReadToEnd();
+                // string content = reader.ReadToEnd();
 
                 // Process CSV content here
 
                 string csvLine = reader.ReadLine();
 
-                int nbline = 5;
+                int nbline = 0;
 
                 gridData.Rows.Clear();
 
                 List<string> text = new List<string>();
 
-                for (int i = 0; i < nbline; i++)
+                for (int i = 0; i <= nbline; i++)
                 {
+                    // import des valeur csv dans le datagrid
+
                     text = csvLine.Split(';').ToList();
-                     
+
                     gridData.Rows.Add(
                     new RowData
                     {
@@ -42,9 +45,29 @@ namespace LocalizationFilesManager
                     });
 
                     csvLine = reader.ReadLine();
+
+                    nbline++;
+
+                    if (reader.EndOfStream)
+                    {
+                        // import de la derniere ligne de valeur dans le datagrid + fin de la boucle
+                        text = csvLine.Split(';').ToList();
+
+                        gridData.Rows.Add(
+                        new RowData
+                        {
+                            Key = text[0],
+                            Languages = new ObservableCollection<string> { text[1], text[2], text[3] },
+                            Comments = ""
+                        });
+
+                        return;
+                    }
+
+
                 }
 
-               // MessageBox.Show("CSV Loaded:\n" + csvLine);
+                // MessageBox.Show("CSV Loaded:\n" + csvLine);
             }
         }
 
@@ -53,8 +76,31 @@ namespace LocalizationFilesManager
             using (StreamWriter writer = new StreamWriter(filePath))
             {
                 string csvContent = ""; // Sampled CSV content
-                writer.Write(csvContent);
-                MessageBox.Show($"Data exported as CSV to: {filePath}");
+                int nbLanguage = gridData.Rows[0].Languages.Count;
+                 
+                // ecriture des valeur
+                for (int i = 0; i < gridData.Rows.Count; i++)
+                {
+                    csvContent = gridData.Rows[i].Key;
+                    writer.Write(csvContent + ";");
+
+
+                    for (int j = 0; j < nbLanguage; j++)
+                    {
+                        csvContent = gridData.Rows[i].Languages[j];
+
+                        writer.Write(csvContent + ";");
+                    }
+
+                    csvContent = gridData.Rows[i].Comments;
+                    writer.Write(csvContent);
+
+                    //changement de ligne
+                    writer.WriteLine();
+
+                }
+
+                 MessageBox.Show($"Data exported as CSV to: {filePath}");
             }
         }
     }
