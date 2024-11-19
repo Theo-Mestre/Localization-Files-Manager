580a5b7460f8adde1583b3ede979a1fd5519bd81
kbabel971 <kevin.jordan.babel@gmail.com>
2024-11-19 12:51:22 +0100

    ajout fonction pour recuperer un dictionnaire de mot

--- LocalizationFilesManager/Core/CSVUtility.cs
+++ LocalizationFilesManager/Core/CSVUtility.cs
@@ -1,17 +1,80 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.IO.Packaging;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Markup;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace LocalizationFilesManager
 {
     public partial class MainWindow
     {
         // CSV Utility Methods
+
+        public Dictionary<string, string> GetLanguage(string language, string filePath)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            List<string> languageText = new List<string>();
+            int sizeLanguageText;
+
+            List<string> text = new List<string>();
+
+            StreamReader reader = new StreamReader(filePath);
+            string csvLine;
+
+            int nbLanguage = 0;
+            int nbline = 0;
+
+            csvLine = reader.ReadLine();
+            languageText = csvLine.Split(';').ToList();
+            sizeLanguageText = languageText.Count;
+
+            // initialisation du nombre de langue existant dans le fichier
+            for (int i = sizeLanguageText - 1; i >= 0; i--)
+            {
+                if (languageText[i] != "Id" && languageText[i] != "comment" && languageText[i] != "")
+                {
+                    nbLanguage++;
+                }
+                else
+                {
+                    // supression des données qui ne sont pas une langue
+                    languageText.RemoveAt(i);
+                }
+            }
+
+            
+            for (int i = 0; i < nbLanguage; i++)
+            {
+                if (language == languageText[i])
+                {
+                    for (int j = 0; j <= nbline; j++)
+                    {
+                        // lecture des données et recupération des données
+                        csvLine = reader.ReadLine();
+                        text = csvLine.Split(';').ToList();
+
+                        // ajout des données dans le dictionnaire
+                        dictionary.Add(text[0], text[i]);
+
+                        nbline++;
+
+                        if (reader.EndOfStream)
+                        {
+                            return dictionary;
+                        }
+
+                    }
+                }
+            }
+
+            return dictionary;
+        }
 
         // import
         private void OnCSVFileOpened(string filePath)
@@ -77,7 +140,7 @@ namespace LocalizationFilesManager
             {
                 string csvContent = ""; // Sampled CSV content
                 int nbLanguage = gridData.Rows[0].Languages.Count;
-                 
+
                 // ecriture des valeur
                 for (int i = 0; i < gridData.Rows.Count; i++)
                 {
@@ -100,7 +163,7 @@ namespace LocalizationFilesManager
 
                 }
 
-                 MessageBox.Show($"Data exported as CSV to: {filePath}");
+                MessageBox.Show($"Data exported as CSV to: {filePath}");
             }
         }
     }
