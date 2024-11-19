558fedc44dc51ead8556653a604f91defbb44e85
Theo-gamedev <g.mestretheo@gmail.com>
2024-11-19 12:41:14 +0100

    Test feature main logic

--- TestFeatures/main.cs
+++ TestFeatures/main.cs
@@ -1,16 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace LocalizationFilesManager
 {
-    internal class main
+    internal class LocalizationFilesExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a file name: ");
+            string filePath = Console.In.ReadLine();
+
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("File path is invalid.");
+                return;
+            }
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return;
+            }
+
+            string extension = Path.GetExtension(filePath);
+
+            Dictionary<string, string> localizationData = new Dictionary<string, string>();
+            switch (extension)
+            {
+                case ".csv":
+                    // localizationData = OnCSVFileOpened(filePath, extension);
+                    Console.WriteLine("This is a resx file.");
+                    break;
+                case ".json":
+                    // localizationData = OnJsonFileOpened(filePath, extension);
+                    Console.WriteLine("This is a json file.");
+                    break;
+                case ".xml":
+                    // localizationData = OnXMLFileOpened(filePath, extension);
+                    Console.WriteLine("This is a json file.");
+                    break;
+                default:
+                    Console.WriteLine("This is an unknown file.");
+                    break;
+            }
+
+            foreach (var item in localizationData)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
         }
     }
 }
